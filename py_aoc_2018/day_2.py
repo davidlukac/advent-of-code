1565e3d02ebe87ae4a4c681982480249b499733b
David Lukac <david.lukac@ferratum.com>
2018-12-05 17:15:55 +0100

    2018 Day#2 Part#2

--- py_aoc_2018/day_2.py
+++ py_aoc_2018/day_2.py
@@ -1,26 +1,53 @@
 from collections import Counter
+from typing import Dict, List
 
 from py_aoc_2018.commons import get_input_file_path, stream_lines_as_str
+
+
+def find_matching(line: str, cache: Dict[str, None], matching_boxes: List[str]):
+    for k in cache.keys():
+        res = ''
+        differences = 0
+        for c1, c2 in zip(line, k):
+            if differences <= 1:
+                if c1 == c2:
+                    res += c1
+                else:
+                    differences += 1
+            else:
+                break
+        if differences == 1:
+            matching_boxes.append(res)
+
+    cache[line] = None
 
 
 def day_2():
     twos = 0
     threes = 0
 
+    # This could be a size-limited cache in which we are comparing, should the input stream of data be endless.
+    cache = dict()
+    matching_boxes = list()
+
     with open(get_input_file_path(2), 'r') as f:
         for line in stream_lines_as_str(f):
+            line = line.strip()
             c = Counter(Counter(line).values())
             if c.get(2):
                 twos += 1
             if c.get(3):
                 threes += 1
 
-    return twos * threes
+            find_matching(line, cache, matching_boxes)
+
+    return twos * threes, matching_boxes
 
 
 def main():
-    checksum = day_2()
+    checksum, matching_boxes = day_2()
     print(f"Checksum of the boxes is {checksum}.")
+    print(f"Matching boxes are {matching_boxes}")
 
 
 if __name__ == '__main__':
--- py_aoc_2018_tests/test_days.py
+++ py_aoc_2018_tests/test_days.py
@@ -3,7 +3,7 @@ import unittest
 import pytest
 
 from py_aoc_2018.day_1 import day_1
-from py_aoc_2018.day_2 import day_2
+from py_aoc_2018.day_2 import day_2, find_matching
 
 
 class TestDay1(unittest.TestCase):
@@ -16,6 +16,25 @@ class TestDay1(unittest.TestCase):
 class TestDay2(unittest.TestCase):
     def test(self):
         assert 8820 == day_2()
+
+    def test_matching(self):
+        data = [
+            'abcde',
+            'fghij',
+            'klmno',
+            'pqrst',
+            'fguij',
+            'axcye',
+            'wvxyz'
+        ]
+
+        cache = {}
+        res = []
+
+        for d in data:
+            find_matching(d, cache, res)
+
+        assert ['fgij'] == res
 
 
 if __name__ == '__main__':
