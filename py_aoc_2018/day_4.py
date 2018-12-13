9a22dc414051e26785db7d7733ca26ed115e9bf2
David Lukac <1215290+davidlukac@users.noreply.github.com>
2018-12-13 15:42:51 +0100

    2018 Day#4 Part#1 Extend `Guard`, `Sleep` and `RawEvent` classes

--- py_aoc_2018/day_4.py
+++ py_aoc_2018/day_4.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
-from datetime import datetime, date
+
+from collections import defaultdict
+from datetime import date, datetime
+from operator import attrgetter
+from typing import Generator, List, Union
+
 from py_aoc_2018.commons import get_input_file_path, stream_lines_as_str
-from typing import List, Generator
 
 
 class Guard:
@@ -9,40 +13,100 @@ class Guard:
         super().__init__()
         self.gid = gid
 
+    @staticmethod
+    def from_string(s: str) -> Guard:
+        # Guard #2083 begins shift
+        return Guard(int(s.strip().split(' ')[1][1:]))
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Guard):
+            return self.__dict__ == o.__dict__
+
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.gid)
+
 
 class Sleep:
-    def __init__(self, d: date, start: int, end: int) -> None:
+    __DEFAULT_END = object()
+
+    def __init__(self, d: date, start: int, end: int = __DEFAULT_END) -> None:
         super().__init__()
-        self.d = d
+        self.date = d
         self.start = start
-        self.end = end
         self._minutes = None
+        if end is self.__DEFAULT_END:
+            self._end = 59
+            self._is_finished = False
+        else:
+            self._end = end
+            self._is_finished = True
 
     @property
     def minutes(self):
         if self._minutes is None:
-            self._minutes = self.end - self.start
+            self._minutes = self._end - self.start
         return self._minutes
+
+    @property
+    def end(self):
+        return self._end
+
+    @end.setter
+    def end(self, end: int):
+        self._end = end
+        self._minutes = None
+        self._is_finished = True
+
+    @property
+    def is_finished(self):
+        return self._is_finished
+
+    @staticmethod
+    def construct(dt: datetime, s: str, sleep: Sleep = None) -> Sleep:
+        # [1518-04-20 00:01] falls asleep
+        # [1518-03-10 00:28] wakes up
+        s = s.strip()
+        if s.startswith('falls'):
+            return Sleep(dt.date(), dt.minute)
+        elif s.startswith('wakes'):
+            assert sleep
+            assert dt.date() == sleep.date
+            sleep.end = dt.minute
+            return sleep
 
 
 class RawEvent:
+    """
+    Processes event string into datetime information and rest of the string representation.
+    """
+
+    DT_FORMAT = '[%Y-%m-%d %H:%M]'
+
     def __init__(self, dt: datetime, raw_event: str) -> None:
         super().__init__()
         self.dt = dt
         self.raw_event = raw_event
 
-    @staticmethod
-    def from_string(s: str) -> RawEvent:
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, RawEvent):
+            return o.__dict__ == self.__dict__
+        else:
+            return False
+
+    def __repr__(self) -> str:
+        return f'{datetime.strftime(self.dt, self.DT_FORMAT)} {self.raw_event}'
+
+    @classmethod
+    def from_string(cls, s: str) -> RawEvent:
         # [1518-03-19 23:47] Guard #2083 begins shift
         # [1518-04-20 00:01] falls asleep
         # [1518-03-10 00:28] wakes up
         s = s.strip()
-        return RawEvent(datetime.strptime(s[0:18], '[%Y-%m-%d %H:%M]'), s[19:].strip())
+        return RawEvent(datetime.strptime(s[0:18], cls.DT_FORMAT), s[19:].strip())
 
 
-class RawEventLog:
-    def __init__(self) -> None:
-        super().__init__()
 
 
 
--- py_aoc_2018_tests/test_day_4.py
+++ py_aoc_2018_tests/test_day_4.py
@@ -1,18 +1,57 @@
-import pytest
-import unittest
 import io
-from py_aoc_2018.commons import get_input_file_path
-from py_aoc_2018.day_4 import Sleep, RawEvent
-from datetime import datetime, date
+import unittest
+from datetime import date, datetime
+
+import pytest
+
+from py_aoc_2018.commons import stream_lines_as_str
+from py_aoc_2018.day_4 import Guard, RawEvent, Sleep, process_str_events
+
+
+class TestSleep(unittest.TestCase):
+    def test_sleep(self):
+        s = Sleep(date(2015, 10, 13), 10, 15)
+        assert s
+        assert s.date == date(2015, 10, 13)
+        assert s.minutes == 5
+
+        s = Sleep(date(2015, 10, 13), 55)
+        assert s.end == 59
+        assert s.minutes == 4
+
+    def test_sleep_setter(self):
+        s = Sleep(date(2015, 10, 13), 55)
+        assert s.end == 59
+        assert s.minutes == 4
+        s.end = 58
+        assert s.end == 58
+        assert s.minutes == 3
+
+    def test_sleep_construct(self):
+        s = Sleep.construct(datetime(2015, 10, 13, 0, 55), 'falls asleep')
+        assert s.date == date(2015, 10, 13)
+        assert s.start == 55
+        assert s.end == 59
+        assert s.minutes == 4
+
+        s = Sleep.construct(datetime(2015, 10, 13, 0, 58), 'wakes up', s)
+        assert s.date == date(2015, 10, 13)
+        assert s.start == 55
+        assert s.end == 58
+        assert s.minutes == 3
+
+    def test_sleep_is_finished(self):
+        s = Sleep.construct(datetime(2015, 10, 13, 0, 55), 'falls asleep')
+        assert not s.is_finished
+        assert s.end == 59
+        assert s.minutes == 4
+        s = Sleep.construct(datetime(2015, 10, 13, 0, 58), 'wakes up', s)
+        assert s.is_finished
+        assert s.end == 58
+        assert s.minutes == 3
 
 
 class TestDay4(unittest.TestCase):
-    def test_event(self):
-        e = Sleep(date(2015, 10, 13), 10, 15)
-        assert e
-        assert e.d == date(2015, 10, 13)
-        assert e.minutes == 5
-
     def test_raw_event(self):
         re = RawEvent.from_string('[1518-11-01 00:00] Guard #10 begins shift')
         assert re.dt == datetime(1518, 11, 1, 0, 0)
@@ -26,31 +65,81 @@ class TestDay4(unittest.TestCase):
         assert re.dt == datetime(1518, 11, 1, 0, 25)
         assert re.raw_event == 'wakes up'
 
-    def test(self):
-        data = [
-            """
-[1518-11-01 00:00] Guard #10 begins shift
-[1518-11-01 00:05] falls asleep
+    def test_raw_event_repr(self):
+        re = RawEvent.from_string('[1518-11-01 00:25] wakes up')
+        assert re.__repr__() == '[1518-11-01 00:25] wakes up'
+
+    def test_raw_event_equal(self):
+        re1 = RawEvent.from_string('[1518-11-01 00:00] Guard #10 begins shift')
+        re2 = RawEvent.from_string('[1518-11-01 00:00] Guard #10 begins shift')
+        assert re1 == re2
+
+    def test_raw_event_processing(self):
+        data = """
 [1518-11-01 00:25] wakes up
-[1518-11-01 00:30] falls asleep
 [1518-11-01 00:55] wakes up
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 23:58] Guard #99 begins shift
 [1518-11-02 00:40] falls asleep
 [1518-11-02 00:50] wakes up
 [1518-11-03 00:05] Guard #10 begins shift
-[1518-11-03 00:24] falls asleep
 [1518-11-03 00:29] wakes up
+[1518-11-03 00:24] falls asleep
 [1518-11-04 00:02] Guard #99 begins shift
-[1518-11-04 00:36] falls asleep
-[1518-11-04 00:46] wakes up
-[1518-11-05 00:03] Guard #99 begins shift
-[1518-11-05 00:45] falls asleep
-[1518-11-05 00:55] wakes up
-            """
+"""
+        r5 = RawEvent.from_string('[1518-11-01 00:00] Guard #10 begins shift')
+        r3 = RawEvent.from_string('[1518-11-01 00:05] falls asleep')
+        r1 = RawEvent.from_string('[1518-11-01 00:25] wakes up')
+        r4 = RawEvent.from_string('[1518-11-01 00:30] falls asleep')
+        r2 = RawEvent.from_string('[1518-11-01 00:55] wakes up')
+        r6 = RawEvent.from_string('[1518-11-01 23:58] Guard #99 begins shift')
+        r7 = RawEvent.from_string('[1518-11-02 00:40] falls asleep')
+        r8 = RawEvent.from_string('[1518-11-02 00:50] wakes up')
+        r9 = RawEvent.from_string('[1518-11-03 00:05] Guard #10 begins shift')
+        r11 = RawEvent.from_string('[1518-11-03 00:24] falls asleep')
+        r10 = RawEvent.from_string('[1518-11-03 00:29] wakes up')
+        r12 = RawEvent.from_string('[1518-11-04 00:02] Guard #99 begins shift')
+
+        stream = io.StringIO(data)
+        sorted_events = process_str_events(stream_lines_as_str(stream))
+        expected = [
+            r5, r3, r1, r4, r2, r6, r7, r8, r9, r11, r10, r12
         ]
 
-        stream = io.StringIO('\n'.join(data))
-        assert (7, 7) == load_claims(stream)[:-1]
+        assert len(expected) == len(sorted_events)
+        assert sorted_events == expected
+
+    def test_guard(self):
+        g1 = Guard(10)
+        g2 = Guard(10)
+        assert g1 == g2
+        assert g1.gid == 10
+        g2 = Guard(20)
+        assert g1 != g2
+
+        g1 = Guard(10)
+        g2 = Guard(10)
+        d = dict()
+        d[g1] = None
+        d[g2] = None
+        assert len(d.keys()) == 1
+        assert list(d.keys())[0].gid == 10
+
+        g1 = Guard(10)
+        g2 = Guard(20)
+        d = dict()
+        d[g1] = None
+        d[g2] = None
+        assert len(d.keys()) == 2
+        assert list(d.keys())[0].gid == 10
+        assert list(d.keys())[1].gid == 20
+
+    def test_guard_factory(self):
+        g1 = Guard.from_string(' Guard #2083 begins shift')
+        assert Guard(2083) == g1
+        assert g1.gid == 2083
 
 
 if __name__ == '__main__':
